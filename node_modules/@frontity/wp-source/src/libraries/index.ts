6447b88371bc1ae746accc43fdd16b7d589a3157
Vic Zer <vicz@hotmail.it>
2021-11-10 18:07:40 +0100

    Remove the now ignored directory node_modules

--- node_modules/@frontity/wp-source/src/libraries/index.ts
+++
@@ -1,6 +0,0 @@
-import { default as Api } from "./api";
-import { default as populate } from "./populate";
-import { default as routeUtils } from "./route-utils";
-import * as responseUtils from "./response-utils";
-
-export { Api, populate, routeUtils, responseUtils };
